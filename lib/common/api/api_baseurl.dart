1e3c5a7ad20f687c343f7d869e8cec7bbcdb71f6
MuhammedNihalcp <nihalriju915@gmail.com>
2022-12-25 04:07:14 +0530

    update api files

---
+++ lib/common/api/api_baseurl.dart
@@ -0,0 +1,3 @@
+class ApiBaseUrl {
+  String baseUrl = 'http://localhost:5000';
+}
---
+++ lib/common/api/api_endsurl.dart
@@ -0,0 +1,10 @@
+class ApiEndsUrl{
+  static String signUp = '/api/v1/auth/signup';
+  static String signIn = '/api/v1/auth/login';
+  static String refresh = '/api/v1/auth/refresh';
+  static String logout = '/api/v1/auth/otp';
+  static String sendOtp = '/auth/otp?email=nihalmuhammed12464@gmail.com';
+  static String verifyOtp = '/api/v1/auth/otp';
+  static String google = '/api/v1/auth/google';
+  static String forgot = '/api/v1/auth/password';
+}
