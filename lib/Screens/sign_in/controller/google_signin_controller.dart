d4a131f1797d504d8faeba19c79d526fc305048c
MuhammedNihalcp <nihalriju915@gmail.com>
2022-12-27 21:03:35 +0530

    update google_signin controller

---
+++ lib/Screens/sign_in/controller/google_signin_controller.dart
@@ -0,0 +1,27 @@
+import 'dart:developer';
+
+import 'package:e_commerce/Screens/navigator_screen/view/view.dart';
+import 'package:e_commerce/service/google_signin/google_sign_in.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSignInContorller extends GetxController {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  bool isLoading = false;
+  void googleSignin() async {
+    isLoading = true;
+    update();
+    await GoogleSignInService.googleSignIn(googleSignIn).then((value) {
+      if (value != null) {
+        log(value.toString());
+        Get.offAll(() => ScreenNavigator());
+        isLoading = false;
+        update();
+      } else {
+        isLoading = false;
+        update();
+        return;
+      }
+    });
+  }
+}
