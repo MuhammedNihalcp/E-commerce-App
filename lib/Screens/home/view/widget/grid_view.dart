8d536baa8a1fcfb252c333bf5e02e15f93ac5455
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-04 20:55:24 +0530

    update product grid view

--- lib/Screens/home/view/widget/grid_view.dart
+++ lib/Screens/home/view/widget/grid_view.dart
@@ -1,5 +1,7 @@
+import 'package:e_commerce/Screens/home/controller/home_controller.dart';
 import 'package:e_commerce/core/size.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProductGridView extends StatelessWidget {
   const ProductGridView({
@@ -13,75 +15,77 @@ class ProductGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      padding: EdgeInsets.zero,
-      physics: const NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 15,
-          mainAxisSpacing: 15,
-          childAspectRatio: 1 / 1.6),
-      itemBuilder: (BuildContext ctx, int index) {
-        return SizedBox(
-          width: width * 0.1,
-          // color: Colors.blue,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      width: width * 0.5,
-                      height: height * 0.22,
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('assets/images/dress/dress3.jpg'),
-                          fit: BoxFit.cover,
+    final productC = Get.put(HomeContorller(context));
+    return GetBuilder<HomeContorller>(
+      builder: (controller) => GridView.builder(
+        padding: EdgeInsets.zero,
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 15,
+            childAspectRatio: 1 / 1.6),
+        itemBuilder: (BuildContext ctx, int index) {
+          return SizedBox(
+            width: width * 0.1,
+            // color: Colors.blue,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: width * 0.5,
+                        height: height * 0.22,
+                        decoration:  BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage('http://172.16.7.123:5000/product/${productC.productList[index].image[0]}'),
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      right: 0,
-                      bottom: 0,
-                      
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.favorite_border_outlined,
+                      Positioned(
+                        right: 0,
+                        bottom: 0,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.favorite_border_outlined,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                   Text(
+                    productC.productList[index].description,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontWeight: FontWeight.w400),
+                  ),
+                  kHeight10,
+                  Row(
+                    children: const [
+                      Text(
+                        '₹ 299',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                const Text(
-                  'The midi dress, which falls between a maxi and a mini, is what everyone needs for situations',
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontWeight: FontWeight.w400),
-                ),
-                kHeight10,
-                Row(
-                  children: const [
-                    Text(
-                      '₹ 299',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        );
-      },
-      itemCount: 10,
+          );
+        },
+        itemCount: 10,
+      ),
     );
   }
 }
