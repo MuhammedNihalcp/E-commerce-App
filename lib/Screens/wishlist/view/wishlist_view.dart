ec2d124141dbac67d9fe5d309c79f19406852827
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-09 11:07:46 +0530

    add wish list screeen file

---
+++ lib/Screens/wishlist/view/wishlist_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WishlistScreen extends StatelessWidget {
+  const WishlistScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [],
+      )),
+    );
+  }
+}
