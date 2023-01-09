8a9d0c79a80ade7239d212a4cf0d85e2724800f8
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-09 10:46:40 +0530

    add wishlist service and add wishlist end url and update this files

---
+++ lib/Screens/wishlist/service/wishlist_service.dart
@@ -0,0 +1,47 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:e_commerce/Screens/wishlist/model/wishlist_mode.dart';
+import 'package:e_commerce/common/api/api_baseurl.dart';
+import 'package:e_commerce/common/api/api_endsurl.dart';
+import 'package:e_commerce/util/dio_exeption/exeptions.dart';
+import 'package:e_commerce/util/dio_interceptor/dio_interceptor.dart';
+
+class WishlistService {
+  final apibaseUrl = ApiBaseUrl();
+  final apiendUrl = ApiEndsUrl();
+  Future<int?> addOrRemoveWishlist(context, productId) async {
+    Dio dio = await ApiInterceptor().getApiUser(context);
+    try {
+      Response response =
+          await dio.post(apibaseUrl.baseUrl + apiendUrl.wishlist, data: {
+        'product': productId,
+      });
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return response.statusCode;
+      } else if (response.statusCode == 204) {
+        response.statusCode;
+      }
+    } on DioError catch (e) {
+      log(e.message);
+      DioException().dioError(e, context);
+    }
+    return null;
+  }
+
+  Future<WishlistModel?> getWishlist(context) async {
+    Dio dio = await ApiInterceptor().getApiUser(context);
+    try {
+      Response response =
+          await dio.get(apibaseUrl.baseUrl + apiendUrl.wishlist);
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        final WishlistModel wishModel = WishlistModel.fromJson(response.data);
+        return wishModel;
+      }
+    } on DioError catch (e) {
+      log(e.message);
+      DioException().dioError(e, context);
+    }
+    return null;
+  }
+}
--- lib/common/api/api_endsurl.dart
+++ lib/common/api/api_endsurl.dart
@@ -1,19 +1,22 @@
-class ApiEndsUrl{
-   String signUp = '/api/v1/auth/signup';
-   String signIn = '/api/v1/auth/login';
-   String refresh = '/api/v1/auth/refresh';
-   String logout = '/api/v1/auth/otp';
+class ApiEndsUrl {
+  String signUp = '/api/v1/auth/signup';
+  String signIn = '/api/v1/auth/login';
+  String refresh = '/api/v1/auth/refresh';
+  String logout = '/api/v1/auth/otp';
   //  String sendOtp = '/auth/otp?email=nihalmuhammed12464@gmail.com';
-   String verifyOtp = '/api/v1/auth/otp';
-   String google = '/api/v1/auth/google';
-   String forgot = '/api/v1/auth/password';
+  String verifyOtp = '/api/v1/auth/otp';
+  String google = '/api/v1/auth/google';
+  String forgot = '/api/v1/auth/password';
 
-   //category
-   String category = '/api/v1/category';
+  //category
+  String category = '/api/v1/category';
 
-   //product
-   String product = '/api/v1/products';
+  //product
+  String product = '/api/v1/products';
 
-   //carousal
-   String carousal = '/api/v1/carousal';
+  //carousal
+  String carousal = '/api/v1/carousal';
+
+  //wishlist
+  String wishlist = '/api/v1/wishlist';
 }
