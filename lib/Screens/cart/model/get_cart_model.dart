4480038bd4f8c1368778c7ce7b0740338a5c3ac1
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-11 13:46:02 +0530

    update get cart model

---
+++ lib/Screens/cart/model/get_cart_model.dart
@@ -0,0 +1,43 @@
+import 'package:e_commerce/Screens/product_view_screen/model/product_model.dart';
+
+class GetCartModel {
+  GetCartModel({
+    required this.id,
+    required this.userId,
+    required this.products,
+    required this.totalPrice,
+    required this.totalDiscount,
+  });
+  String id;
+  String userId;
+  List<ProductElement> products;
+  int totalPrice;
+  dynamic totalDiscount;
+}
+
+class ProductElement {
+  ProductElement({
+    required this.product,
+    required this.size,
+    required this.qty,
+    required this.price,
+    required this.discountPrice,
+    required this.id,
+  });
+
+  ProductModel product;
+  String size;
+  int qty;
+  int price;
+  dynamic discountPrice;
+  String id;
+
+  factory ProductElement.fromJson(Map<String, dynamic> json) => ProductElement(
+        product: ProductModel.fromJson(json["product"]),
+        size: json["size"],
+        qty: json["qty"],
+        price: json["price"],
+        discountPrice: json["discountPrice"],
+        id: json["_id"],
+      );
+}
