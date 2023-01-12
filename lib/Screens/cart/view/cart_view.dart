2f0bceac9f5c04ae0b761c6d6598786622b68462
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-12 14:30:07 +0530

    cart add add and remove file on widget folder

--- lib/Screens/cart/view/cart_view.dart
+++ lib/Screens/cart/view/cart_view.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/Screens/cart/view/widget/add_remove_button/add_remove_button.dart';
 import 'package:e_commerce/Screens/cart/view/widget/cart_bottom_button/cart_bottom_button.dart';
 import 'package:e_commerce/Screens/cart/view/widget/cart_image_detail/cart_image_details.dart';
 import 'package:e_commerce/core/size.dart';
@@ -37,10 +38,6 @@ class ScreenOrder extends StatelessWidget {
             )
           ],
         ),
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.menu),
-        ),
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(height * 0.04),
           child: const SizedBox(),
@@ -68,81 +65,9 @@ class ScreenOrder extends StatelessWidget {
                           height: height,
                         ),
                         kHeight10,
-                        Row(
-                          children: [
-                            Container(
-                              height: height * 0.05,
-                              decoration: BoxDecoration(
-                                  color:
-                                      const Color.fromARGB(255, 238, 237, 237),
-                                  border: Border.all(color: colorLightGrey),
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(10))),
-                              child: Row(
-                                children: [
-                                  ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(10)),
-                                        backgroundColor: const Color.fromARGB(
-                                            255, 238, 237, 237),
-                                        minimumSize:
-                                            Size(width * 0.05, height * 0.05)),
-                                    onPressed: () {},
-                                    child: const Icon(
-                                      Icons.delete_outline,
-                                      color: colorBlack,
-                                    ),
-                                  ),
-                                  Container(
-                                    width: width * 0.13,
-                                    // height: height * 0.05,
-                                    color: const Color.fromARGB(
-                                        255, 223, 220, 220),
-                                    child: const Center(
-                                        child: Text(
-                                      '2',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    )),
-                                  ),
-                                  ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(10)),
-                                          backgroundColor: const Color.fromARGB(
-                                              255, 238, 237, 237),
-                                          minimumSize: Size(
-                                              width * 0.05, height * 0.05)),
-                                      onPressed: () {},
-                                      child: const Icon(
-                                        Icons.add,
-                                        color: colorBlack,
-                                      ))
-                                ],
-                              ),
-                            ),
-                            kWidth10,
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10)),
-                                  backgroundColor: colorWhite,
-                                  minimumSize: Size(width * 0.2, height * 0.05),
-                                  side: const BorderSide(
-                                    width: 1,
-                                    color: colorLightGrey,
-                                  )),
-                              onPressed: () {},
-                              child: const Text(
-                                'Delete',
-                                style: TextStyle(color: colorBlack),
-                              ),
-                            )
-                          ],
+                        AddAndRemoveButton(
+                          height: height,
+                          width: width,
                         ),
                         Row(
                           children: [
@@ -169,5 +94,3 @@ class ScreenOrder extends StatelessWidget {
     );
   }
 }
-
-
---
+++ lib/Screens/cart/view/widget/add_remove_button/add_remove_button.dart
@@ -0,0 +1,85 @@
+import 'package:e_commerce/core/size.dart';
+import 'package:e_commerce/core/text_style.dart';
+import 'package:flutter/material.dart';
+
+class AddAndRemoveButton extends StatelessWidget {
+  const AddAndRemoveButton({
+    Key? key,
+    required this.height,
+    required this.width,
+  }) : super(key: key);
+
+  final double height;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          height: height * 0.05,
+          decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 238, 237, 237),
+              border: Border.all(color: colorLightGrey),
+              borderRadius: const BorderRadius.all(Radius.circular(10))),
+          child: Row(
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    backgroundColor: const Color.fromARGB(255, 238, 237, 237),
+                    minimumSize: Size(width * 0.05, height * 0.05)),
+                onPressed: () {},
+                child: const Icon(
+                  Icons.delete_outline,
+                  color: colorBlack,
+                ),
+              ),
+              Container(
+                width: width * 0.13,
+                // height: height * 0.05,
+                color: const Color.fromARGB(255, 223, 220, 220),
+                child: const Center(
+                    child: Text(
+                  '2',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      backgroundColor: const Color.fromARGB(255, 238, 237, 237),
+                      minimumSize: Size(width * 0.05, height * 0.05)),
+                  onPressed: () {},
+                  child: const Icon(
+                    Icons.add,
+                    color: colorBlack,
+                  ))
+            ],
+          ),
+        ),
+        kWidth10,
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              backgroundColor: colorWhite,
+              minimumSize: Size(width * 0.2, height * 0.05),
+              side: const BorderSide(
+                width: 1,
+                color: colorLightGrey,
+              )),
+          onPressed: () {},
+          child: const Text(
+            'Delete',
+            style: TextStyle(color: colorBlack),
+          ),
+        )
+      ],
+    );
+  }
+}
