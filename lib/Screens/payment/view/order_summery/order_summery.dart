25432447632cfdd3be412e6779064f93efa85185
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-15 16:07:53 +0530

    update order details like add detail cart in that file

--- lib/Screens/payment/view/order_summery/order_summery.dart
+++ lib/Screens/payment/view/order_summery/order_summery.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/Screens/payment/controller/order_summery_controller/order_summery_controller.dart';
+import 'package:e_commerce/Screens/payment/view/order_summery/widget/order_details/order_details.dart';
 import 'package:e_commerce/core/size.dart';
 import 'package:e_commerce/core/text_style.dart';
 import 'package:flutter/material.dart';
@@ -55,76 +56,9 @@ class ScreenOrderSummery extends StatelessWidget {
         child: GetBuilder<OrderSummeryController>(
           builder: (controller) => Column(
             children: [
-              CustomCard(
-                width: double.infinity,
-                height: height * 0.22,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        // crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Deliver to:',
-                            style: TextStyle(
-                                color: colorBlack,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16),
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: colorWhite,
-                                side: const BorderSide(color: colorGrey)),
-                            onPressed: () {},
-                            child: const Text(
-                              'Change',
-                              style: TextStyle(color: colorVailet),
-                            ),
-                          ),
-                        ],
-                      ),
-                      kHeight5,
-                      Row(
-                        children: [
-                          const Text(
-                            'Muhammed Nihal',
-                            style: TextStyle(
-                                color: colorBlack,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15),
-                          ),
-                          kWidth5,
-                          CustomCard(
-                            width: 50,
-                            height: 25,
-                            borderRadius: 5,
-                            color: const Color.fromARGB(255, 236, 235, 235),
-                            child: const Text(
-                              'Home',
-                              style: TextStyle(
-                                  color: colorGrey,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          )
-                        ],
-                      ),
-                      kHeight10,
-                      const Text(
-                        '445,Kondotty,Edavannapara,678542',
-                        style: TextStyle(
-                          color: colorBlack,
-                          fontSize: 15,
-                        ),
-                      ),
-                      kHeight10,
-                      const Text('6238961357'),
-                    ],
-                  ),
-                ),
-              )
+              OrderDetails(
+                height: height,
+              ),
             ],
           ),
         ),
---
+++ lib/Screens/payment/view/order_summery/widget/order_details/order_details.dart
@@ -0,0 +1,87 @@
+import 'package:e_commerce/core/size.dart';
+import 'package:e_commerce/core/text_style.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_custom_cards/flutter_custom_cards.dart';
+
+class OrderDetails extends StatelessWidget {
+  const OrderDetails({
+    Key? key,
+    required this.height,
+  }) : super(key: key);
+
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomCard(
+      width: double.infinity,
+      height: height * 0.22,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Deliver to:',
+                  style: TextStyle(
+                      color: colorBlack,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: colorWhite,
+                      side: const BorderSide(color: colorGrey)),
+                  onPressed: () {},
+                  child: const Text(
+                    'Change',
+                    style: TextStyle(color: colorVailet),
+                  ),
+                ),
+              ],
+            ),
+            kHeight5,
+            Row(
+              children: [
+                const Text(
+                  'Muhammed Nihal',
+                  style: TextStyle(
+                      color: colorBlack,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
+                ),
+                kWidth5,
+                CustomCard(
+                  width: 50,
+                  height: 25,
+                  borderRadius: 5,
+                  color: const Color.fromARGB(255, 236, 235, 235),
+                  child: const Text(
+                    'Home',
+                    style: TextStyle(
+                        color: colorGrey, fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+            kHeight10,
+            const Text(
+              '445,Kondotty,Edavannapara,678542',
+              style: TextStyle(
+                color: colorBlack,
+                fontSize: 15,
+              ),
+            ),
+            kHeight10,
+            const Text('6238961357'),
+          ],
+        ),
+      ),
+    );
+  }
+}
