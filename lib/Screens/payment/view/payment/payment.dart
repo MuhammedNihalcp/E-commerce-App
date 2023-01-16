f3a9584f2c596ddefe87555cd859384232a95a62
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-16 13:01:05 +0530

    create payment folder

---
+++ lib/Screens/payment/view/payment/payment.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/widgets.dart';
+
+class ScreenPayment extends StatelessWidget {
+  const ScreenPayment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
