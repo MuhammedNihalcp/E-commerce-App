123ec643b8723411ced32364ca06120c46d53d43
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-16 10:07:39 +0530

    delivary details chage to widget

--- lib/Screens/payment/view/order_summery/widget/order_productDetails/order_product_details.dart
+++ lib/Screens/payment/view/order_summery/widget/order_productDetails/order_product_details.dart
@@ -140,33 +140,44 @@ class OrderProductDetails extends StatelessWidget {
               width: width,
             ),
             kHeight10,
-            RichText(
-              text: const TextSpan(
-                text: '',
-                style: TextStyle(color: colorBlack),
-                children: <TextSpan>[
-                  TextSpan(
-                      text: 'Delivary by Fri jan 20 |',
-                      style: TextStyle(color: colorBlack)),
-                  TextSpan(
-                    text: ' FREE Delivery  ',
-                    style: TextStyle(
-                      color: Colors.green,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  TextSpan(
-                    text: '₹40',
-                    style: TextStyle(
-                      color: colorGrey,
-                      decoration: TextDecoration.lineThrough,
-                    ),
-                  )
-                ],
-              ),
-            ),
+            const DelivaryDetails(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class DelivaryDetails extends StatelessWidget {
+  const DelivaryDetails({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: const TextSpan(
+        text: '',
+        style: TextStyle(color: colorBlack),
+        children: <TextSpan>[
+          TextSpan(
+              text: 'Delivary by Fri jan 20 |',
+              style: TextStyle(color: colorBlack)),
+          TextSpan(
+            text: ' FREE Delivery  ',
+            style: TextStyle(
+              color: Colors.green,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          TextSpan(
+            text: '₹40',
+            style: TextStyle(
+              color: colorGrey,
+              decoration: TextDecoration.lineThrough,
+            ),
+          )
+        ],
       ),
     );
   }
