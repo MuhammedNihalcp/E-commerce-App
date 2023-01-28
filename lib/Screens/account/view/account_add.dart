6738891f486887f24811225c8e533f5234488bdf
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-28 09:34:00 +0530

    add new add account widget in widget folder

--- lib/Screens/account/view/account_add.dart
+++ lib/Screens/account/view/account_add.dart
@@ -1,7 +1,7 @@
 import 'package:e_commerce/Screens/account/controller/account_controller.dart';
-import 'package:e_commerce/core/size.dart';
+
 import 'package:e_commerce/core/text_style.dart';
-import 'package:e_commerce/util/textfield/textfield.dart';
+import 'package:e_commerce/Screens/account/view/widget/add_account_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -20,7 +20,6 @@ class AddAccountPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // automaticallyImplyLeading: false,
         backgroundColor: colorVailet,
         centerTitle: true,
         title: Row(
@@ -44,7 +43,6 @@ class AddAccountPage extends StatelessWidget {
             )
           ],
         ),
-
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(height * 0.04),
           child: const SizedBox(),
@@ -57,89 +55,10 @@ class AddAccountPage extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(15.0),
             child: Form(
-              child: Column(
-                children: [
-                  kHeight10,
-                  TextFromFieldWidget(
-                    textController: accountC.tittleC,
-                    nullText: 'Tittle is Empty',
-                    obscureText: false,
-                    icon: const Icon(Icons.person),
-                    text: 'Tittle',
-                  ),
-                  kHeight10,
-                  TextFromFieldWidget(
-                    textController: accountC.fullNameC,
-                    nullText: 'Fullname is Empty',
-                    obscureText: false,
-                    icon: const Icon(Icons.person),
-                    text: 'Full Name',
-                  ),
-                  kHeight10,
-                  TextFromFieldWidget(
-                    textController: accountC.phoneC,
-                    nullText: 'Phone number is Empty',
-                    obscureText: false,
-                    icon: const Icon(Icons.phone),
-                    text: 'Phone Number',
-                  ),
-                  kHeight10,
-                  TextFromFieldWidget(
-                    textController: accountC.pinC,
-                    nullText: 'PinCode is Empty',
-                    obscureText: false,
-                    icon: const Icon(Icons.pin),
-                    text: 'PinCode',
-                  ),
-                  kHeight10,
-                  TextFromFieldWidget(
-                    textController: accountC.stateC,
-                    nullText: 'State is Empty',
-                    obscureText: false,
-                    icon: const Icon(Icons.public),
-                    text: 'State',
-                  ),
-                  kHeight10,
-                  TextFromFieldWidget(
-                    textController: accountC.placeC,
-                    nullText: 'Place is Empty',
-                    obscureText: false,
-                    icon: const Icon(Icons.location_on),
-                    text: 'Place',
-                  ),
-                  kHeight10,
-                  TextFromFieldWidget(
-                    textController: accountC.addressC,
-                    nullText: 'Address is Empty',
-                    obscureText: false,
-                    icon: const Icon(Icons.contact_mail),
-                    text: 'Address',
-                  ),
-                  kHeight10,
-                  TextFromFieldWidget(
-                    textController: accountC.landmarkC,
-                    nullText: 'LandMark is Empty',
-                    obscureText: false,
-                    icon: const Icon(Icons.emoji_flags),
-                    text: 'Delivary Location',
-                  ),
-                  kHeight10,
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(buttonColor),
-                      fixedSize: MaterialStateProperty.all(
-                        Size(width * 0.8, height * 0.08),
-                      ),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                    ),
-                    onPressed: () {},
-                    child: Text('Submit'),
-                  )
-                ],
+              child: FormWidgets(
+                accountC: accountC,
+                width: width,
+                height: height,
               ),
             ),
           ),
@@ -148,3 +67,5 @@ class AddAccountPage extends StatelessWidget {
     );
   }
 }
+
+
---
+++ lib/Screens/account/view/widget/add_account_widget.dart
@@ -0,0 +1,106 @@
+import 'package:e_commerce/Screens/account/controller/account_controller.dart';
+import 'package:e_commerce/core/size.dart';
+import 'package:e_commerce/core/text_style.dart';
+import 'package:e_commerce/util/textfield/textfield.dart';
+import 'package:flutter/material.dart';
+
+class FormWidgets extends StatelessWidget {
+  const FormWidgets({
+    super.key,
+    required this.accountC,
+    required this.width,
+    required this.height,
+  });
+
+  final AcountController accountC;
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        kHeight10,
+        TextFromFieldWidget(
+          textController: accountC.tittleC,
+          nullText: 'Tittle is Empty',
+          obscureText: false,
+          icon: const Icon(Icons.person),
+          text: 'Tittle',
+        ),
+        kHeight10,
+        TextFromFieldWidget(
+          textController: accountC.fullNameC,
+          nullText: 'Fullname is Empty',
+          obscureText: false,
+          icon: const Icon(Icons.person),
+          text: 'Full Name',
+        ),
+        kHeight10,
+        TextFromFieldWidget(
+          textController: accountC.phoneC,
+          nullText: 'Phone number is Empty',
+          obscureText: false,
+          icon: const Icon(Icons.phone),
+          text: 'Phone Number',
+        ),
+        kHeight10,
+        TextFromFieldWidget(
+          textController: accountC.pinC,
+          nullText: 'PinCode is Empty',
+          obscureText: false,
+          icon: const Icon(Icons.pin),
+          text: 'PinCode',
+        ),
+        kHeight10,
+        TextFromFieldWidget(
+          textController: accountC.stateC,
+          nullText: 'State is Empty',
+          obscureText: false,
+          icon: const Icon(Icons.public),
+          text: 'State',
+        ),
+        kHeight10,
+        TextFromFieldWidget(
+          textController: accountC.placeC,
+          nullText: 'Place is Empty',
+          obscureText: false,
+          icon: const Icon(Icons.location_on),
+          text: 'Place',
+        ),
+        kHeight10,
+        TextFromFieldWidget(
+          textController: accountC.addressC,
+          nullText: 'Address is Empty',
+          obscureText: false,
+          icon: const Icon(Icons.contact_mail),
+          text: 'Address',
+        ),
+        kHeight10,
+        TextFromFieldWidget(
+          textController: accountC.landmarkC,
+          nullText: 'LandMark is Empty',
+          obscureText: false,
+          icon: const Icon(Icons.emoji_flags),
+          text: 'Delivary Location',
+        ),
+        kHeight10,
+        ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(buttonColor),
+            fixedSize: MaterialStateProperty.all(
+              Size(width * 0.8, height * 0.08),
+            ),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          onPressed: () {},
+          child: Text('Submit'),
+        )
+      ],
+    );
+  }
+}
