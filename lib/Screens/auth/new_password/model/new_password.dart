3c7722b6b3129ac82675ed5d72dd12a2ba3cf77e
MuhammedNihalcp <nihalriju915@gmail.com>
2023-02-04 18:15:34 +0530

    create new password model

---
+++ lib/Screens/auth/new_password/model/new_password.dart
@@ -0,0 +1,16 @@
+class NewPasswordModel {
+  final String email;
+  final String password;
+
+  NewPasswordModel({
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      "email": email,
+      "password": password,
+    };
+  }
+}
