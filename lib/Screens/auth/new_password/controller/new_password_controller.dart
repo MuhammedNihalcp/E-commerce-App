626209b322238115826969dd72ac9f02c8d5d7c2
MuhammedNihalcp <nihalriju915@gmail.com>
2023-02-04 19:08:53 +0530

    update controller

--- lib/Screens/auth/new_password/controller/new_password_controller.dart
+++ lib/Screens/auth/new_password/controller/new_password_controller.dart
@@ -1,9 +1,35 @@
+import 'package:e_commerce/Screens/auth/new_password/model/new_password.dart';
+import 'package:e_commerce/Screens/auth/new_password/service/new_password_service.dart';
+import 'package:e_commerce/Screens/navigator_screen/view/view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 
-class NewPasswordController extends GetxController{
- 
- TextEditingController confrompasswordController = TextEditingController();
- TextEditingController newpasswordController = TextEditingController();
+class NewPasswordController extends GetxController {
+  TextEditingController confrompasswordController = TextEditingController();
+  TextEditingController newpasswordController = TextEditingController();
 
+  FlutterSecureStorage storage = const FlutterSecureStorage();
+
+  bool isLoading = false;
+  final newpasswordS = NewPasswordService();
+
+  void addPassword(String email) async {
+    isLoading = true;
+    update();
+
+    final newpasswordModel = NewPasswordModel(
+      email: email,
+      password: newpasswordController.text.trim(),
+    );
+    newpasswordS.addNewPassword(newpasswordModel).then((value) {
+      if (value != null) {
+        Get.offAll(ScreenNavigator());
+        isLoading = false;
+        update();
+      }
+      isLoading = false;
+      update();
+    });
+  }
 }
