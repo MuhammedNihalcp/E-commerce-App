cf41333a23bc6338ba649b399199979ead218e17
MuhammedNihalcp <nihalriju915@gmail.com>
2023-02-04 18:57:49 +0530

    add new new password contorller

---
+++ lib/Screens/auth/new_password/controller/new_password_controller.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class NewPasswordController extends GetxController{
+ 
+ TextEditingController confrompasswordController = TextEditingController();
+ TextEditingController newpasswordController = TextEditingController();
+
+}
--- lib/Screens/auth/new_password/view/new_password_view.dart
+++ lib/Screens/auth/new_password/view/new_password_view.dart
@@ -1,15 +1,39 @@
+import 'package:e_commerce/Screens/auth/new_password/controller/new_password_controller.dart';
+import 'package:e_commerce/util/textfield/textfield.dart';
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
 
 class ScreenNewPassword extends StatelessWidget {
-  const ScreenNewPassword({super.key,required this.email,});
+  ScreenNewPassword({
+    super.key,
+    required this.email,
+  });
   final String email;
+  final newPasswordC = Get.put(NewPasswordController());
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('New password'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            TextFromFieldWidget(
+              textController: newPasswordC.newpasswordController,
+              nullText: 'New password empty',
+              obscureText: true,
+              icon: Icon(Icons.visibility_off),
+              text: 'New Password',
+              textInputType: TextInputType.name,
+            ),
+            TextFromFieldWidget(
+                textController: newPasswordC.confrompasswordController,
+                nullText: 'Conform password empty',
+                obscureText: true,
+                icon: Icon(Icons.visibility_off),
+                text: 'Conform Password',
+                textInputType: TextInputType.name),
+          ],
+        ),
       ),
     );
   }
