3273ff49df5af184f9a95bcd74de1d61d71c6a43
MuhammedNihalcp <nihalriju915@gmail.com>
2023-02-13 11:51:47 +0530

    update new password view

--- lib/Screens/auth/new_password/view/new_password_view.dart
+++ lib/Screens/auth/new_password/view/new_password_view.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/Screens/auth/new_password/controller/new_password_controller.dart';
+import 'package:e_commerce/core/size.dart';
 import 'package:e_commerce/core/text_style.dart';
 import 'package:e_commerce/util/textfield/textfield.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,8 @@ class ScreenNewPassword extends StatelessWidget {
   double width = Get.size.width;
   double height = Get.size.height;
 
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +30,7 @@ class ScreenNewPassword extends StatelessWidget {
             child: Column(
               children: [
                 const Text(
-                  'New Password',
+                  'Reset Password',
                   maxLines: 2,
                   style: TextStyle(
                       fontSize: 30,
@@ -51,84 +54,134 @@ class ScreenNewPassword extends StatelessWidget {
       ),
       body: SafeArea(
         child: GetBuilder<NewPasswordController>(
-          builder: (controller) => Column(
-            children: [
-              TextFormField(
-                controller: newPasswordC.newpasswordController,
-                keyboardType: TextInputType.visiblePassword,
-                validator: (value) => newPasswordC.passwordValdation(value),
-                obscureText: newPasswordC.obscureText,
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
+          builder: (controller) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  kHeight20,
+                  const Text(
+                    'Enter New Password',
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: colorVailet,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  kHeight20,
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      'Your new Password must be different from previously used password',
+                      // maxLines: 2,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 73, 67, 67),
+                        fontWeight: FontWeight.w300,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: height * 0.1,
+                  ),
+                  TextFormField(
+                    controller: newPasswordC.newpasswordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    validator: (value) => newPasswordC.passwordValdation(value),
+                    obscureText: newPasswordC.obscureText,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          newPasswordC.visibility();
+                        },
+                        icon: newPasswordC.icon,
+                        color: colorBlack,
+                      ),
+                      label: const Text(
+                        'New Password',
+                        style: TextStyle(color: colorBlack),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(20)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(20)),
+                      disabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(20)),
+                      focusedErrorBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.red),
+                          borderRadius: BorderRadius.circular(20)),
+                      errorBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.red),
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  ),
+                  kHeight10,
+                  TextFormField(
+                    controller: newPasswordC.confrompasswordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    validator: (value) =>
+                        newPasswordC.confirmpasswordValdation(value),
+                    obscureText: newPasswordC.obscureText,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          newPasswordC.visibility();
+                        },
+                        icon: newPasswordC.icon,
+                        color: colorBlack,
+                      ),
+                      label: const Text(
+                        'Password',
+                        style: TextStyle(color: colorBlack),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(20)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(20)),
+                      disabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(20)),
+                      focusedErrorBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.red),
+                          borderRadius: BorderRadius.circular(20)),
+                      errorBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.red),
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  ),
+                  kHeight20,
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(buttonColor),
+                      fixedSize: MaterialStateProperty.all(
+                        Size(width * 0.8, height * 0.08),
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
                     onPressed: () {
-                      newPasswordC.visibility();
+                      if (formKey.currentState!.validate()) {
+                        newPasswordC.addPassword(email);
+                      }
                     },
-                    icon: newPasswordC.icon,
-                    color: colorBlack,
+                    child: const Text(
+                      'CONTINUE',
+                      style: buttonStyle,
+                    ),
                   ),
-                  label: const Text(
-                    'New Password',
-                    style: TextStyle(color: colorBlack),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(20)),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(20)),
-                  disabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(20)),
-                  focusedErrorBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.red),
-                      borderRadius: BorderRadius.circular(20)),
-                  errorBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.red),
-                      borderRadius: BorderRadius.circular(20)),
-                ),
+                ],
               ),
-              TextFormField(
-                controller: newPasswordC.confrompasswordController,
-                keyboardType: TextInputType.visiblePassword,
-                validator: (value) =>
-                    newPasswordC.confirmpasswordValdation(value),
-                obscureText: newPasswordC.obscureText,
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      newPasswordC.visibility();
-                    },
-                    icon: newPasswordC.icon,
-                    color: colorBlack,
-                  ),
-                  label: const Text(
-                    'Password',
-                    style: TextStyle(color: colorBlack),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(20)),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(20)),
-                  disabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(20)),
-                  focusedErrorBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.red),
-                      borderRadius: BorderRadius.circular(20)),
-                  errorBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.red),
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  newPasswordC.addPassword(email);
-                },
-                child: Text('continue'),
-              )
-            ],
+            ),
           ),
         ),
       ),
