94cd3cdd2ca0f08534c98baf63c5676f976946c0
MuhammedNihalcp <nihalriju915@gmail.com>
2023-02-14 10:23:48 +0530

    change base url

--- lib/common/api/api_baseurl.dart
+++ lib/common/api/api_baseurl.dart
@@ -1,6 +1,7 @@
 class ApiBaseUrl {
   // String baseUrl = 'http://172.16.7.123:5000';
-  String baseUrl = 'http://10.4.6.82:5000';
+  String baseUrl = 'http://192.168.207.196:5000';
+
 
   // String baseUrl = 'http://172.16.5.75:5000';
 }
