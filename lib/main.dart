63e64d50e2bf901fafc304861a211de687a5f919
MuhammedNihalcp <nihalriju915@gmail.com>
2023-03-02 07:01:57 +0530

    add google plugin,dependancies and json files on android folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:e_commerce/Screens/product_view_screen/view/product_view.dart';
 import 'package:e_commerce/Screens/splash_screen/view/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
+import firebase_core
 import flutter_secure_storage_macos
 import package_info_plus_macos
 import path_provider_foundation
@@ -13,6 +14,7 @@ import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
