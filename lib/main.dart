f67c5f4ac67841d239b22b1dd9264499a79230cb
MuhammedNihalcp <nihalriju915@gmail.com>
2023-03-03 10:25:11 +0530

    update files

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
